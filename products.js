1e5e19877924c23650bc7955248c5425f048911c
dream <2537687014@qq.com>
2026-06-12 22:42:27 +0800

    1

--- products.js
+++ products.js
@@ -1,8 +1,8 @@
-﻿// 由 DreamShopManager 自动生成
+﻿// 由 DreamShopManager 内容引擎自动生成，请勿手动修改
 const ALL_PRODUCTS = {
   "new_software_44800339": {
     "id": "new_software_44800339",
-    "name": "全新发布的软件商品",
+    "name": "Office_VBS_Player",
     "price": "$00.00",
     "logo": "logo/shop3.png",
     "time": "2026-06-10",
@@ -13,81 +13,114 @@ const ALL_PRODUCTS = {
       "通道一 夸克下载（推荐，速度最快）": "https://www.4399.com/"
     },
     "authType": "开源软件 (免费)",
-    "desc": "1.最强测试\r\n2.test的测试",
+    "desc": "Office系列办公组件的VBS管理助手\r\n基于COM接口提供一键部署各种功能的服务，安装使用助手即可集成管理VBS的文件夹，您可以自由地增减，使用对应的模块来一键办公。",
     "imgs": [
       "ShopPicture/shop2.png",
       "ShopPicture/shop1.png"
     ],
     "features": [
-      "1.免费",
-      "2.小体积"
+      "一键部署Office VBS脚本",
+      "集成管理VBS文件夹",
+      "模块化设计，灵活增减功能",
+      "兼容Office 2016及以上版本和大部分Wps版本",
+      "内测版暂不开放下载"
     ],
     "specs": {}
   },
   "product_05675141": {
     "id": "product_05675141",
-    "name": "阿里巴巴的父亲",
-    "price": "$0.00",
-    "logo": "logo/default.png",
+    "name": "KeyMouseSimulator",
+    "price": "$00.00",
+    "logo": "logo/20260409233346.png",
     "time": "2026-06-11",
     "size": "0.00 MB",
     "ver": "V1.0.0",
     "env": "Win 10 | Win 11",
     "downloadLinks": {},
     "authType": "免费软件",
-    "desc": "请输入商品详情描述...",
-    "imgs": [],
-    "features": [],
+    "desc": "键盘动作模拟，一组支持上千次的模拟键盘\r\n游戏辅助和办公自动化的神器",
+    "imgs": [
+      "ShopPicture/shop13.png",
+      "ShopPicture/shop14.png",
+      "ShopPicture/shop15.png"
+    ],
+    "features": [
+      "ESC强制停止，空格键暂停/继续",
+      "可自定义延迟",
+      "可自定义连招宏"
+    ],
     "specs": {}
   },
   "new_software_43627592": {
     "id": "new_software_43627592",
-    "name": "阿里巴巴的爷爷",
-    "price": "$2.99",
-    "logo": "logo/shop11.png",
+    "name": "MemeMaster",
+    "price": "$99.99",
+    "logo": "logo/biaoqing.png",
     "time": "2026-06-10",
-    "size": "2.00 MB",
-    "ver": "V1.0.2",
+    "size": "27.00 MB",
+    "ver": "V1.0.0（内测版）",
     "env": "Win 7 | Win 10",
     "downloadLinks": {},
-    "authType": "一次性购买，永久使用",
-    "desc": "ali88的爷爷",
-    "imgs": [],
+    "authType": "免费软件",
+    "desc": "MemeMaster是表情包制作大师\r\n畅绘各种充满想象力的表情，你的创意表达加速神器",
+    "imgs": [
+      "ShopPicture/shop10.png",
+      "ShopPicture/shop11.png",
+      "ShopPicture/shop12.png"
+    ],
     "features": [
-      "弯道超车的神器"
+      "小白专用，无需专业设计功底",
+      "日常斗图，打造专属情绪语言",
+      "支持 PNG（透明背景）、BMP、JPEG等格式输出",
+      "内测版暂不开放下载"
     ],
     "specs": {}
   },
   "new_software_43229090": {
     "id": "new_software_43229090",
-    "name": "新世纪金额的大风",
-    "price": "$99.99",
-    "logo": "logo/shop7.png",
+    "name": "汉字和Unicode互转",
+    "price": "$00.00",
+    "logo": "logo/U+.png",
     "time": "2026-06-10",
-    "size": "2.00 MB",
-    "ver": "V1.0.2",
-    "env": "Win 10 | Win 11",
+    "size": "0.72 MB",
+    "ver": "V1.0.0",
+    "env": "Win 7 | Win 10 | Win 11",
     "downloadLinks": {},
-    "authType": "一次性购买，永久使用",
-    "desc": "这是test用的",
-    "imgs": [],
-    "features": [],
+    "authType": "免费软件",
+    "desc": "汉字和Unicode即时互转工具\r\n快速实现汉字和Unicode编码即时互转",
+    "imgs": [
+      "ShopPicture/shop8.png",
+      "ShopPicture/shop9.png"
+    ],
+    "features": [
+      "单击使用即时互转",
+      "小体积无需安装，下载即用"
+    ],
     "specs": {}
   },
   "new_software_28511216": {
     "id": "new_software_28511216",
-    "name": "植物大战僵尸",
-    "price": "$99.99",
-    "logo": "logo/default.png",
+    "name": "PVZ Player",
+    "price": "$00.00",
+    "logo": "logo/duola.png",
     "time": "2026-06-10",
-    "size": "0.00 MB",
+    "size": "0.89 MB",
     "ver": "V1.0.0",
     "env": "Win 10 | Win 11",
     "downloadLinks": {},
     "authType": "免费软件",
-    "desc": "请输入商品详情描述...",
-    "imgs": [],
-    "features": [],
+    "desc": "PVZ系列通用游戏修改助手\r\n基于PVZ进程提供修改，下载即可体验无限阳光、无冷却种植。",
+    "imgs": [
+      "ShopPicture/shop4.png",
+      "ShopPicture/shop5.png",
+      "ShopPicture/shop6.png"
+    ],
+    "features": [
+      "✓输入使用无限阳光",
+      "✓锁定指定值阳光",
+      "✓种植植物无冷却",
+      "✓兼容PVZ原版和PVZHE杂交版"
+    ],
     "specs": {}
   }
 };
