e615b43eff1f34af242e3f6d0ea40749f94a5c1c
dream <2537687014@qq.com>
2026-06-12 10:22:15 +0800

    1

---
+++ products.js
@@ -0,0 +1,93 @@
+﻿// 由 DreamShopManager 自动生成
+const ALL_PRODUCTS = {
+  "new_software_44800339": {
+    "id": "new_software_44800339",
+    "name": "全新发布的软件商品",
+    "price": "$00.00",
+    "logo": "logo/shop3.png",
+    "time": "2026-06-10",
+    "size": "1.70 MB",
+    "ver": "V1.0.2",
+    "env": "Win 10 | Win 11",
+    "downloadLinks": {
+      "通道一 夸克下载（推荐，速度最快）": "https://www.4399.com/"
+    },
+    "authType": "开源软件 (免费)",
+    "desc": "1.最强测试\r\n2.test的测试",
+    "imgs": [
+      "ShopPicture/shop2.png",
+      "ShopPicture/shop1.png"
+    ],
+    "features": [
+      "1.免费",
+      "2.小体积"
+    ],
+    "specs": {}
+  },
+  "product_05675141": {
+    "id": "product_05675141",
+    "name": "阿里巴巴的父亲",
+    "price": "$0.00",
+    "logo": "logo/default.png",
+    "time": "2026-06-11",
+    "size": "0.00 MB",
+    "ver": "V1.0.0",
+    "env": "Win 10 | Win 11",
+    "downloadLinks": {},
+    "authType": "免费软件",
+    "desc": "请输入商品详情描述...",
+    "imgs": [],
+    "features": [],
+    "specs": {}
+  },
+  "new_software_43627592": {
+    "id": "new_software_43627592",
+    "name": "阿里巴巴的爷爷",
+    "price": "$2.99",
+    "logo": "logo/shop11.png",
+    "time": "2026-06-10",
+    "size": "2.00 MB",
+    "ver": "V1.0.2",
+    "env": "Win 7 | Win 10",
+    "downloadLinks": {},
+    "authType": "一次性购买，永久使用",
+    "desc": "ali88的爷爷",
+    "imgs": [],
+    "features": [
+      "弯道超车的神器"
+    ],
+    "specs": {}
+  },
+  "new_software_43229090": {
+    "id": "new_software_43229090",
+    "name": "新世纪金额的大风",
+    "price": "$99.99",
+    "logo": "logo/shop7.png",
+    "time": "2026-06-10",
+    "size": "2.00 MB",
+    "ver": "V1.0.2",
+    "env": "Win 10 | Win 11",
+    "downloadLinks": {},
+    "authType": "一次性购买，永久使用",
+    "desc": "这是test用的",
+    "imgs": [],
+    "features": [],
+    "specs": {}
+  },
+  "new_software_28511216": {
+    "id": "new_software_28511216",
+    "name": "植物大战僵尸",
+    "price": "$99.99",
+    "logo": "logo/default.png",
+    "time": "2026-06-10",
+    "size": "0.00 MB",
+    "ver": "V1.0.0",
+    "env": "Win 10 | Win 11",
+    "downloadLinks": {},
+    "authType": "免费软件",
+    "desc": "请输入商品详情描述...",
+    "imgs": [],
+    "features": [],
+    "specs": {}
+  }
+};
