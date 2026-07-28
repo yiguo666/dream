97371844482ee9eababcaabff087d9642dfaa47c
dream <2537687014@qq.com>
2026-07-28 16:41:06 +0800

    Auto commit 2026-07-28 16:41:06

--- softwares.js
+++ softwares.js
@@ -58,7 +58,7 @@
       "weight": "⭐⭐⭐⭐⭐⭐⭐",
       "mainImage": "logo/logo_20260714233422130.png",
       "detailImages": "logo/logo_20260714233438110.png,logo/logo_20260714233440080.png,logo/logo_20260714233442078.png,logo/logo_20260727120202582.png,",
-      "resourcePath": "https://pan.quark.cn/s/9f76e12bed24;https://pan.quark.cn/s/50271e181527",
+      "resourcePath": "https://pan.quark.cn/s/d4743abba3b4;https://pan.quark.cn/s/cbb7a2aa1d20",
       "isRecommend": true,
       "version": "1.0.1",
       "size": "323.5kb",
