1716cf76c7242854dc083894190391d8b72c8520
dream <2537687014@qq.com>
2026-07-28 14:00:47 +0800

    Auto commit 2026-07-28 14:00:47

--- softwares.js
+++ softwares.js
@@ -12,7 +12,10 @@
       "version": "1.0.0",
       "size": "52.50MB",
       "environment": "Win 7 | Win 10 | Win 11",
-      "description": "植物大战僵尸的经典作品，必玩塔防游戏。\r\n极富策略性的小游戏，集成了即时战略、塔防战斗和卡片收集等要素。\r\n在游戏中，玩家通过栽种植物，武装自己的家园，把僵尸阻挡在入侵的道路上，不同的敌人，不同的玩法构成五种不同的游戏模式，加之黑夜、浓雾以及泳池之类的障碍增加了游戏挑战性。\r\n（p.s.当前版本已经完成了汉化设置和对应版本兼容的性能优化，下载解压即可畅玩，无需安装注册表。）"
+      "description": "植物大战僵尸的经典作品，必玩塔防游戏。\r\n极富策略性的小游戏，集成了即时战略、塔防战斗和卡片收集等要素。\r\n在游戏中，玩家通过栽种植物，武装自己的家园，把僵尸阻挡在入侵的道路上，不同的敌人，不同的玩法构成五种不同的游戏模式，加之黑夜、浓雾以及泳池之类的障碍增加了游戏挑战性。\r\n（p.s.当前版本已经完成了汉化设置和对应版本兼容的性能优化，下载解压即可畅玩，无需安装注册表。）",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P733",
@@ -26,7 +29,10 @@
       "version": "1.5.0",
       "size": "473KB",
       "environment": "Win 10 | Win 11",
-      "description": "本工具支持市面上绝大部分植物大战僵尸官方版本及部分二创的修改版，同时也适配 64 位 Windows 10 / 11 系统。\r\n关于启动报错“缺少 .NET Runtime”的解决方案，如果遇到启动失败或弹窗提示“You must install .NET Desktop Runtime”，说明你的系统缺少该组件或版本较低，请直接点击弹窗中的“Download it now”链接，根据提示下载安装即可解决。\r\n为了兼顾不同用户，这里提供了两个版本：\r\n通道一（推荐）：体积较小，适合绝大多数用户，双击即可运行。如遇启动失败，请尝试通道二。\r\n通道二（兼容版）：体积较大，内置自适应运行环境，当通道一无法正常使用时，请下载此版本。\r\nWindows 7 特别提示： 使用兼容版前，请确保已安装 KB2533623 系统补丁，否则可能无法正常启动。"
+      "description": "本工具支持市面上绝大部分植物大战僵尸官方版本及部分二创的修改版，同时也适配 64 位 Windows 10 / 11 系统。\r\n关于启动报错“缺少 .NET Runtime”的解决方案，如果遇到启动失败或弹窗提示“You must install .NET Desktop Runtime”，说明你的系统缺少该组件或版本较低，请直接点击弹窗中的“Download it now”链接，根据提示下载安装即可解决。\r\n为了兼顾不同用户，这里提供了两个版本：\r\n通道一（推荐）：体积较小，适合绝大多数用户，双击即可运行。如遇启动失败，请尝试通道二。\r\n通道二（兼容版）：体积较大，内置自适应运行环境，当通道一无法正常使用时，请下载此版本。\r\nWindows 7 特别提示： 使用兼容版前，请确保已安装 KB2533623 系统补丁，否则可能无法正常启动。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P870",
@@ -40,7 +46,10 @@
       "version": "6.0.0",
       "size": "1.11GB",
       "environment": "Win XP | Win 7 | Win 10 | Win 11",
-      "description": "八十年代黑帮横行的罪恶城藏满犯罪机遇，刑满十五年的汤米奉弗雷利家族之命前往当地交易毒品，不料交易遭伏击全盘失败。在家族首领桑尼的要求下，汤米着手追查伏击主谋，并借机在罪恶城发展自身势力。\r\n城市布局由东岛、西岛及虾岛等多个岛屿构成，包含罪城海滩、大洋海滩、市中心、小哈瓦那等风格各异的区域，整体呈现迈阿密与南佛罗里达的都市风貌。\r\n玩家可在庞大的城市地图中自由探索，执行主线与支线任务，从事抢劫、暗杀等活动，并驾驶汽车、摩托车、飞机等多种载具。\r\n(p.s.下载一是罪恶都市汉化版，下载二是魔改奥特曼模型汉化版)"
+      "description": "八十年代黑帮横行的罪恶城藏满犯罪机遇，刑满十五年的汤米奉弗雷利家族之命前往当地交易毒品，不料交易遭伏击全盘失败。在家族首领桑尼的要求下，汤米着手追查伏击主谋，并借机在罪恶城发展自身势力。\r\n城市布局由东岛、西岛及虾岛等多个岛屿构成，包含罪城海滩、大洋海滩、市中心、小哈瓦那等风格各异的区域，整体呈现迈阿密与南佛罗里达的都市风貌。\r\n玩家可在庞大的城市地图中自由探索，执行主线与支线任务，从事抢劫、暗杀等活动，并驾驶汽车、摩托车、飞机等多种载具。\r\n(p.s.下载一是罪恶都市汉化版，下载二是魔改奥特曼模型汉化版)",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P466",
@@ -48,13 +57,16 @@
       "category": "🎯 原创",
       "weight": "⭐⭐⭐⭐⭐⭐⭐",
       "mainImage": "logo/logo_20260714233422130.png",
-      "detailImages": "logo/logo_20260714233438110.png,logo/logo_20260714233440080.png,logo/logo_20260714233442078.png",
+      "detailImages": "logo/logo_20260714233438110.png,logo/logo_20260714233440080.png,logo/logo_20260714233442078.png,logo/logo_20260727120202582.png,",
       "resourcePath": "https://pan.quark.cn/s/9f76e12bed24;https://pan.quark.cn/s/50271e181527",
       "isRecommend": true,
-      "version": "1.0.0",
+      "version": "1.0.1",
       "size": "323.5kb",
       "environment": "Win 10 | Win 11",
-      "description": "历时三个月，我把它做出来了，那个只存在于想象中的播放器，没有广告推送，没有社交干扰，只有精心打磨的桌面可视化动效和纯净的音质输出。\r\n这是音律的艺术，全网独家的音乐播放器重新定义了本地音乐的打开方式。\r\n创新性制作了歌词跳转模式，可以玩的音乐模式！\r\n告别繁琐的媒体库，追求控制感与独特性的迷你音乐播放器，不联网、不扫描，保护隐私的同时实现毫秒级响应。"
+      "description": "没有广告推送，没有社交干扰，只有精心打磨的桌面可视化动效和纯净的音质输出。\r\n全网独家，重新定义了本地音乐的打开方式。\r\n歌词跳转，可以玩的音乐模式！\r\n告别繁琐的媒体库，追求控制感与独特性的迷你音乐播放器，不联网、不扫描，保护隐私的同时实现毫秒级响应。",
+      "detailPageType": "自定义模板",
+      "detailPagePath": "projects/lrcplayer.html",
+      "customPagePath": "projects/lrcplayer.html"
     },
     {
       "id": "P303",
@@ -64,11 +76,14 @@
       "mainImage": "logo/YI~H`}85LAZK[RS}1{IY5`T.png",
       "detailImages": "logo/]R9R4EH4~G$}JX0$RGSMPXY.png,logo/3LEQ3R)U2AG7HT0}06UX1N3.png",
       "resourcePath": "https://pan.quark.cn/s/b6d1b9e20194;https://pan.quark.cn/s/a73c21d269f9",
-      "isRecommend": false,
+      "isRecommend": true,
       "version": "1.0.0",
       "size": "8.5MB",
       "environment": "Win 10 | Win 11",
-      "description": "图片与图标互转，拖拽即识别。\r\n从 .ico 文件里提取高清 PNG，还是将任意图片一键转为专业级 .ico 图标，拖拽即可完成。\r\n操作零门槛，支持图片与图标互转，无需手动选择转换方向，拖入文件自动匹配对应功能。\r\n轻量单文件，无需安装，完美解决图标尺寸、格式适配问题。"
+      "description": "图片与图标互转，拖拽即识别。\r\n从 .ico 文件里提取高清 PNG，还是将任意图片一键转为专业级 .ico 图标，拖拽即可完成。\r\n操作零门槛，支持图片与图标互转，无需手动选择转换方向，拖入文件自动匹配对应功能。\r\n轻量单文件，无需安装，完美解决图标尺寸、格式适配问题。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P979",
@@ -82,7 +97,10 @@
       "version": "1.0.0",
       "size": "2.60MB",
       "environment": "Win 7 | Win 10 | Win 11",
-      "description": "玩家可以选择不同的角色进行对战，包括草莓、桃子、垃圾桶、电球、咖啡豆和茄子等。\r\n每个角色都拥有独特的远程技能、大招和战斗特长。"
+      "description": "玩家可以选择不同的角色进行对战，包括草莓、桃子、垃圾桶、电球、咖啡豆和茄子等。\r\n每个角色都拥有独特的远程技能、大招和战斗特长。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P837",
@@ -96,7 +114,10 @@
       "version": "2.8.3",
       "size": "38.7MB",
       "environment": "Win 7 | Win 10 | Win 11",
-      "description": "Snipaste 是一个简单但强大的贴图工具，同时也可以执行截屏、标注等功能。 \r\n快捷键（默认为 F1）、鼠标左键 单击托盘图标。\r\n拥有回放截图记录、逐像素控制光标移动 、 像素级控制截取区域 、放大镜、取色、贴图等功能。"
+      "description": "Snipaste 是一个简单但强大的贴图工具，同时也可以执行截屏、标注等功能。 \r\n快捷键（默认为 F1）、鼠标左键 单击托盘图标。\r\n拥有回放截图记录、逐像素控制光标移动 、 像素级控制截取区域 、放大镜、取色、贴图等功能。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P554",
@@ -110,7 +131,10 @@
       "version": "1.0.0",
       "size": "5.68 MB",
       "environment": "Win 7 | Win 10 | Win 11",
-      "description": "混乱大枪战，一款刺激好玩的游戏。\r\n拿起你手中的枪去击败对手吧，小心炸弹哦，可是会把你自己炸飞的。\r\n游戏加载完后点击CAMPAIGN，再选择关卡。\r\n点击选择单人或双人模式，最后点击右下角Start Level即可开始游戏。"
+      "description": "混乱大枪战，一款刺激好玩的游戏。\r\n拿起你手中的枪去击败对手吧，小心炸弹哦，可是会把你自己炸飞的。\r\n游戏加载完后点击CAMPAIGN，再选择关卡。\r\n点击选择单人或双人模式，最后点击右下角Start Level即可开始游戏。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P142",
@@ -124,7 +148,10 @@
       "version": "1.0.0",
       "size": "20.7 MB",
       "environment": "Win 7 | Win 10",
-      "description": "最经典的仙侠游戏大作\r\n内置存档编辑器和经典存档集合"
+      "description": "最经典的仙侠游戏大作\r\n内置存档编辑器和经典存档集合",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P936",
@@ -138,7 +165,10 @@
       "version": "1.0.0",
       "size": "19.6 MB",
       "environment": "Win 7 | Win 10 | Win 11",
-      "description": "《勇者之路》系列续作，玩家扮演大陆冒险者。\r\n可以收服功能各异的精灵伙伴，双人配合闯过多层奇幻关卡。\r\n讨伐魔物 BOSS，追寻传说中冒险王遗留的宝藏。"
+      "description": "《勇者之路》系列续作，玩家扮演大陆冒险者。\r\n可以收服功能各异的精灵伙伴，双人配合闯过多层奇幻关卡。\r\n讨伐魔物 BOSS，追寻传说中冒险王遗留的宝藏。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P188",
@@ -152,7 +182,10 @@
       "version": "1.4.1",
       "size": "1.7MB",
       "environment": "Win 2000 |Win XP |Win 7 | Win 10 | Win 11",
-      "description": "一款文件搜索工具，基于名称实时定位文件和目录。\r\n体积小巧，界面简洁易用，快速建立索引，快速搜索，同时占用极低的系统资源，实时跟踪文件变化。\r\n在搜索框输入文字，它就会只显示过滤后的文件和目录。\r\nEverything搜索只基于文件和文件夹的名称，创建数据库速度超快。"
+      "description": "一款文件搜索工具，基于名称实时定位文件和目录。\r\n体积小巧，界面简洁易用，快速建立索引，快速搜索，同时占用极低的系统资源，实时跟踪文件变化。\r\n在搜索框输入文字，它就会只显示过滤后的文件和目录。\r\nEverything搜索只基于文件和文件夹的名称，创建数据库速度超快。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P687",
@@ -162,11 +195,14 @@
       "mainImage": "logo/SA]O3$5SAYRE_`LVR37M0]3.png",
       "detailImages": "logo/%X6WO482TPS_XHX)FF[5OBR.png,logo/_YSMOH}%G0JCFB2~YX0`TNO.png",
       "resourcePath": "",
-      "isRecommend": false,
+      "isRecommend": true,
       "version": "1.0.0",
       "size": "",
       "environment": "Win 10 | Win 11",
-      "description": "反复输入相同指令而烦恼？让键鼠模拟器帮你解放双手。\r\n\r\n只需在软件中设置好按键序列（如 iiiijj）和间隔延迟，将鼠标移动到目标窗口，3 秒后它就会自动开始执行。\r\n中途想停下来，按 Esc 立刻终止，按空格随时暂停。自动填表、重复录入等多种场景。轻量无后台，用完即走。"
+      "description": "反复输入相同指令而烦恼？让键鼠模拟器帮你解放双手。\r\n\r\n只需在软件中设置好按键序列（如 iiiijj）和间隔延迟，将鼠标移动到目标窗口，3 秒后它就会自动开始执行。\r\n中途想停下来，按 Esc 立刻终止，按空格随时暂停。自动填表、重复录入等多种场景。轻量无后台，用完即走。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P350",
@@ -178,9 +214,12 @@
       "resourcePath": "https://pan.quark.cn/s/69abefa36f2f;https://pan.quark.cn/s/cba9270156eb",
       "isRecommend": false,
       "version": "1.0.0",
-      "size": "",
+      "size": "207.2kb",
       "environment": "Win 10 | Win 11",
-      "description": "专注于金额数字与中文大写互转的桌面效率工具。\r\n它的核心是解决财务、报销、合同等文件填写等场景下，手动转换大写金额易出错、效率低的问题。\r\n毫秒级精准转换支持一键复制，彻底告别“壹贰叁”写错重写的烦恼。\r\n针对小写数字，可灵活切换千分位分隔符（逗号/空格）和小数精度（保留整数/1位/2位），满足不同财务系统的录入规范，支持一键导出TXT日志。"
+      "description": "专注于金额数字与中文大写互转的桌面效率工具。\r\n它的核心是解决财务、报销、合同等文件填写等场景下，手动转换大写金额易出错、效率低的问题。\r\n毫秒级精准转换支持一键复制，彻底告别“壹贰叁”写错重写的烦恼。\r\n针对小写数字，可灵活切换千分位分隔符（逗号/空格）和小数精度（保留整数/1位/2位），满足不同财务系统的录入规范，支持一键导出TXT日志。",
+      "detailPageType": "",
+      "detailPagePath": "",
+      "customPagePath": ""
     },
     {
       "id": "P241",
@@ -194,7 +233,10 @@
       "version": "1.0.0",
       "size": "55.6MB",
       "environment": "Win 7 | Win 8 | Win 10 | Win 11",
-      "description": "工坊原创项目大多依赖于微软环境.net8.0框架，系统里没有更新版本的小伙伴们可以在这里下载DotNet8的一键下载器，程序会自动检测、运行并安装，一键完成！\r\n下载器内置安装包，单击第一个按钮一键完成所有部署。\r\n如果实在无法运行且手动下载也失败的，可访问微软官方备用下载链接：https://dotnet.microsoft.com/en-us/download/dotnet/8.0"
+      "description": "工坊原创项目大多依赖于微软环境.net8.0框架，系统里没有更新版本的小伙伴们可以在这里下载DotNet8的一键下载器，程序会自动检测、运行并安装，一键完成！\r\n下载器内置安装包，单击第一个按钮一键完成所有部署。\r\n如果实在无法运行且手动下载也失败的，可访问微软官方备用下载链接：https://dotnet.microsoft.com/en-us/download/dotnet/8.0",
+      "detailPageType": "通用模板 (tail.html)",
+      "detailPagePath": "",
+      "customPagePath": ""
     }
   ],
   "announcements": [
@@ -285,5 +327,9 @@
         "text": "完成 WebMaster 个人主页新设计"
       }
     ]
-  }
+  },
+  "hotRecommendIds": [
+    "P466",
+    "P733"
+  ]
 };
